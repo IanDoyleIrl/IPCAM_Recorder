98099df8a0bf63d1c8c3c28d9052b2da81c6cbe4
ian <ianpauldoyle@gmail.com>
2013-01-14 22:36:00 +0000

    Updated

--- src/main/java/org/test/cameraMonitor/util/HibernateUtil.java
+++ src/main/java/org/test/cameraMonitor/util/HibernateUtil.java
@@ -10,6 +10,7 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
+            System.out.println("Trying local xml file!");
             SessionFactory sessionFactory = new Configuration()
                     .configure("hibernate.cfg.xml")
                     .buildSessionFactory();
